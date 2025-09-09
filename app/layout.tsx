d640e8dd397f14e2e65ad12162a9dc457fe3acc0
xixiboliya <linboxi123@163.com>
2025-09-09 23:49:04 +0800

    modify:vercel deploy feats

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>
+        {children}
+        <SpeedInsights />
+        <Analytics />
+      </body>
+    </html>
+  );
+}
